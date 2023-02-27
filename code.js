9f8d0375a4b9927b92a3c2ae99d5c05540ed29f6
WlodekM <wlodzio.maly@gmail.com>
2023-02-27 09:43:14 +0200

    jc code

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ function validate(){
     let current  = document.getElementById("password").value /* Get password from form */
     if (current === "[password here]") {
         //valid password
+        let pswdw = document.getElementById("pswd")
+        pswdw.hide()
         //open window "id='secure'"
     } else {
         document.getElementById("wrong").innerHTML="Wrong password!"
