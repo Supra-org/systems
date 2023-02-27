24178794c31b8f38b6fc8981de5acceae682edf8
WlodekM <wlodzio.maly@gmail.com>
2023-02-27 09:48:53 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,8 +2,9 @@ function validate(){
     let current  = document.getElementById("password").value /* Get password from form */
     if (current === "[password here]") {
         //valid password
-        let pswdw = document.getElementById("pswd")
-        pswdw.hide()
+        document.getElementById("wrong").innerHTML=""
+        let pswdw = document.getElementById("password")
+        pswdw.parents('div.window').hide();
         //open window "id='secure'"
     } else {
         document.getElementById("wrong").innerHTML="Wrong password!"
