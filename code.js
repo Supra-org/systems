7d7f88de8dd04869b5a71743e40e47d06cab82a9
WlodekM <wlodzio.maly@gmail.com>
2023-02-27 09:54:02 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
-function validate(){
+$('.windowclose').on("click", function () {
     let current  = document.getElementById("password").value /* Get password from form */
     if (current === "[password here]") {
         //valid password
         document.getElementById("wrong").innerHTML=""
         let pswdw = document.getElementById("password")
-        pswdw.parents('div.window').hide();
+        $(this).parents('div.window').hide();
         //open window "id='secure'"
     } else {
         document.getElementById("wrong").innerHTML="Wrong password!"
         document.getElementById("password").value = ""
     }
-}
+});
